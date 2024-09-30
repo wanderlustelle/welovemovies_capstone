db56522161ade44c231a63a44ce34981055f5eaf
Wanderlust Elle <102614781+wanderlustelle@users.noreply.github.com>
2024-09-30 15:06:11 -0700

    Initial commit

---
+++ knexfile.js
@@ -0,0 +1,53 @@
+const path = require("path");
+
+require("dotenv").config();
+
+const {
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+} = process.env;
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    ssl: {
+      rejectUnauthorized: false // This will allow connections without requiring SSL certificates to be valid.
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    ssl: {
+      rejectUnauthorized: false // This will allow connections without requiring SSL certificates to be valid.
+    }
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+};
